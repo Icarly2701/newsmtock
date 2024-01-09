c26c4287787dd2c9d10385220c322fc2d80a1334
Icarly2701 <dbtjdsla0127@gmail.com>
2024-01-09 22:57:05 +0900

    init: 설정 이후 첫 테스트

--- post/src/test/java/com/newstock/post/PostApplicationTests.java
+++ post/src/test/java/com/newstock/post/PostApplicationTests.java
@@ -1,10 +1,10 @@
-package com.newsmtock.newsmtock;
+package com.newstock.post;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NewsmtockApplicationTests {
+class PostApplicationTests {
 
 	@Test
 	void contextLoads() {
