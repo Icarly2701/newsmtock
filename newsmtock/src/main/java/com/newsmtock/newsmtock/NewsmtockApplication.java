3921bc99ceac7a7584db2744c7c03ea75aa6051d
Icarly2701 <dbtjdsla0127@gmail.com>
2024-01-09 19:55:19 +0900

    init: 프로젝트 생성

---
+++ newsmtock/src/main/java/com/newsmtock/newsmtock/NewsmtockApplication.java
@@ -0,0 +1,13 @@
+package com.newsmtock.newsmtock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsmtockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsmtockApplication.class, args);
+	}
+
+}
---
+++ newsmtock/src/test/java/com/newsmtock/newsmtock/NewsmtockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newsmtock.newsmtock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsmtockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
