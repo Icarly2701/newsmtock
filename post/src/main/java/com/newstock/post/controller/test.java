4f02e385a3b307d79dcb4f801e55014359d5b926
Icarly2701 <dbtjdsla0127@gmail.com>
2024-01-23 20:24:48 +0900

    feat: 기본적인 타임리프 구현 및 하이퍼링크 구현

---
+++ post/src/main/java/com/newstock/post/controller/test.java
@@ -0,0 +1,36 @@
+package com.newstock.post.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class test {
+
+    @GetMapping("/")
+    public String aa(){
+        return "mainpage";
+    }
+    @GetMapping("/login")
+    public String login(){
+        return "loginpage";
+    }
+    @GetMapping("/signup")
+    public String signup(){
+        return "signuppage";
+    }
+    @GetMapping("/mypage")
+    public String mypage(){
+        return "mypage";
+    }
+    @GetMapping("/post")
+    public String posts(){
+        return "postnewspage";
+    }
+    @GetMapping("/news")
+    public String news(){
+        return "postnewspage";
+    }
+    @PostMapping("/search")
+    public String search(){return "searchapage";}
+}
